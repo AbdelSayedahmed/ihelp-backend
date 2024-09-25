6164fff8f955b91e5ae307cd2e338ebaaf202ee0
AbdelSayedahmed <abdelrahman.s2003@icloud.com>
2024-09-25 19:45:06 -0400

    updated requesters query

--- queries/requestersQueries.js
+++ queries/requestersQueries.js
@@ -1,30 +1,68 @@
-const getAllRequesters = `
-    SELECT * FROM requesters;
-`;
+const db = require("../db/db-config.js");
 
-const getRequesterById = `
-    SELECT * FROM requesters WHERE id = $1;
-`;
+const getAllRequesters = async () => {
+  try {
+    const allRequesters = await db.any("SELECT * FROM requesters");
+    console.log(allRequesters);
+    return allRequesters;
+  } catch (error) {
+    throw error;
+  }
+};
 
-const createRequester = `
-    INSERT INTO requesters (name, email, phone, organization, address) 
-    VALUES ($1, $2, $3, $4, $5) RETURNING *;
-`;
+const getRequesterById = async (id) => {
+  try {
+    const requester = await db.one("SELECT * FROM requesters WHERE id=$1", id);
+    return requester;
+  } catch (error) {
+    throw error;
+  }
+};
 
-const updateRequester = `
-    UPDATE requesters 
-    SET name = $1, email = $2, phone = $3, organization = $4, address = $5
-    WHERE id = $6 RETURNING *;
-`;
+const createRequester = async (requester) => {
+  const { name, email, phone, organization, address } = requester;
 
-const deleteRequester = `
-    DELETE FROM requesters WHERE id = $1;
-`;
+  try {
+    const newRequester = await db.one(
+      "INSERT INTO requesters (name, email, phone, organization, address) VALUES($1, $2, $3, $4, $5) RETURNING *",
+      [name, email, phone, organization, address]
+    );
+    return newRequester;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const deleteRequester = async (id) => {
+  try {
+    const deletedRequester = await db.one(
+      "DELETE FROM requesters WHERE id = $1 RETURNING *",
+      id
+    );
+    return deletedRequester;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const updateRequester = async (id, requester) => {
+  const { name, email, phone, organization, address } = requester;
+
+  try {
+    const updatedRequester = await db.one(
+      "UPDATE requesters SET name=$1, email=$2, phone=$3, organization=$4, address=$5 WHERE id=$6 RETURNING *",
+      [name, email, phone, organization, address, id]
+    );
+    return updatedRequester;
+  } catch (error) {
+    throw error;
+  }
+};
 
 module.exports = {
   getAllRequesters,
   getRequesterById,
   createRequester,
-  updateRequester,
   deleteRequester,
+  updateRequester,
 };
