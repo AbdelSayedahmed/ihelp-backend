590ccbb7a60a148f67e74cbed8f5c13f85a6385c
AbdelSayedahmed <abdelrahman.s2003@icloud.com>
2024-09-28 11:29:33 -0400

    updated schema

--- db/schema.sql
+++ db/schema.sql
@@ -5,72 +5,117 @@ CREATE DATABASE ihelp_db;
 
 CREATE TABLE requesters (
     id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
-    email TEXT NOT NULL,
-    phone TEXT NOT NULL,
-    organization TEXT NOT NULL,
-    address TEXT NOT NULL
-);
-
-CREATE TABLE volunteers (
-    id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
-    email TEXT NOT NULL,
-    age INT NOT NULL,
-    organization TEXT NOT NULL,
-    points INT NOT NULL
-);
+    org_id REFERENCES organizations(id),
+    name VARCHAR(255) NOT NULL,
+    phone VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    is_active BOOLEAN DEFAULT TRUE,
+)
 
 CREATE TABLE requests (
     id SERIAL PRIMARY KEY,
-    organization TEXT NOT NULL,
-    volunteer_id INT REFERENCES volunteers(id),
-    requester_id INT REFERENCES requesters(id),
-    status TEXT NOT NULL,
+    org_id INT NOT NULL,
+    volunteer_id INT NOT NULL,
+    requester_id INT NOT NULL,
+    status_id INT NOT NULL,
     description TEXT NOT NULL,
-    due_date TIMESTAMP NOT NULL
-);
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
+
+CREATE TABLE request_task (
+    id SERIAL PRIMARY KEY,
+    requester_id INT NOT NULL,
+    organization_id INT NOT NULL,
+    request_id INT NOT NULL,
+    point_earnings INT NOT NULL,
+    due_date DATE NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
 
 CREATE TABLE organizations (
     id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
-    phone TEXT NOT NULL,
-    address TEXT NOT NULL,
+    address_id REFERENCES addresses(id),
+    phone VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
     description TEXT NOT NULL,
-    email TEXT NOT NULL
-);
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
+
+CREATE TABLE addresses (
+    id SERIAL PRIMARY KEY,
+    street VARCHAR(255) NOT NULL,
+    city VARCHAR(255) NOT NULL,
+    state VARCHAR(255) NOT NULL,
+    zip_code VARCHAR(255) NOT NULL,
+    country VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
 
 CREATE TABLE rewards (
     id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
+    name VARCHAR(255) NOT NULL,
     description TEXT NOT NULL,
-    organization_id INT REFERENCES organizations(id),
-    points_required INT NOT NULL
-);
+    organization_id INT NOT NULL,
+    points_required INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
 
-CREATE TABLE volunteer_rewards (
+CREATE TABLE rewards_earned (
     id SERIAL PRIMARY KEY,
-    reward_id INT REFERENCES rewards(id),
-    volunteer_id INT REFERENCES volunteers(id)
-);
+    reward_id REFERENCES rewards(id),
+    volunteer_id REFERENCES volunteers(id),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
 
-CREATE TABLE game_progress (
+CREATE TABLE volunteers (
     id SERIAL PRIMARY KEY,
-    volunteer_id INT REFERENCES volunteers(id),
-    current_level INT NOT NULL
-);
+    organization_id REFERENCES organizations(id),
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    age INT NOT NULL,
+    points_earned INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    is_active BOOLEAN DEFAULT TRUE,
+)
+
+CREATE TABLE badges_earned (
+    id SERIAL PRIMARY KEY,
+    volunteer_id REFERENCES volunteers(id),
+    badge_id REFERENCES badges(id),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
 
 CREATE TABLE badges (
     id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
+    name VARCHAR(255) NOT NULL,
     description TEXT NOT NULL,
-    icon_url TEXT NOT NULL,
-    required_tasks TEXT NOT NULL
-);
+    img_url VARCHAR(255) NOT NULL,
+    requirement TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
 
-CREATE TABLE volunteer_badges (
+CREATE TABLE assigned_tasks (
     id SERIAL PRIMARY KEY,
-    volunteer_id INT REFERENCES volunteers(id),
-    badge_id INT REFERENCES badges(id)
-);
+    request_task_id REFERENCES request_task(id),
+    volunteer_id REFERENCES volunteers(id),
+    task_progress_id REFERENCES task_progress(id),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
 
+CREATE TABLE task_progress (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
