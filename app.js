52e21f192c496474787dce518c2cbc156fa4850f
MarcusB86 <marcusb08@gmail.com>
2024-10-01 14:56:35 -0400

    volunteer routes and queries complete and functional

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 
 // Route Imports
 const requestersController = require("./controllers/requestersController");
-// const volunteersController = require("./controllers/volunteersController");
+const volunteersController = require("./controllers/volunteersController");
 const organizationsController = require("./controllers/organizationsController");
 
 // Configuration
@@ -19,7 +19,7 @@ app.get("/", (req, res) => res.send("Welcome to iHelp"));
 
 // Routes
 app.use("/requesters", requestersController);
-// app.use("/volunteers", volunteersController);
+app.use("/volunteers", volunteersController);
 app.use("/organizations", organizationsController);
 
 // 404 Page
