3a035e38b62166424bb81ff649cf7addab1c290d
MarcusB86 <marcusb08@gmail.com>
2024-10-01 21:56:22 -0400

    Status routes complete and functional

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const requestersController = require("./controllers/requestersController");
 const volunteersController = require("./controllers/volunteersController");
 const organizationsController = require("./controllers/organizationsController");
 const badgesController = require("./controllers/badgesController");
-
+const requestsController = require("./controllers/requestsController");
+const statusesController = require("./controllers/statusesController");
 // Configuration
 const app = express();
 
@@ -23,6 +24,8 @@ app.use("/requesters", requestersController);
 app.use("/volunteers", volunteersController);
 app.use("/organizations", organizationsController);
 app.use("/badges", badgesController);
+app.use("/requests", requestsController);
+app.use("/statuses", statusesController);
 
 // 404 Page
 app.get("*", (req, res) => res.status(404).send("Page not found"));
