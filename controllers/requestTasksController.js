94c65f8e16cf710a51795f83ba32c3d4b107e57c
kurtCoding <kjulien7790@gmail.com>
2024-10-02 15:41:08 -0400

    changed taskcontroller name to requestTaskController to match schema, have working route to getAllRequestTask and getRequestTaskById via controller and queries

---
+++ controllers/requestTasksController.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const requestTasks = express.Router();
+const {
+    getAllRequestTasks,
+    getRequestTaskById,
+    // createRequestTask,
+    // updateRequestTask,
+    // deleteRequestTask
+} = require("../queries/requestTasksQueries");
+
+requestTasks.get("/", async (req, res) => {
+    try {
+        const allRequestTasks = await getAllRequestTasks();
+        res.status(200).json(allRequestTasks);
+    } catch (error) {
+        res.status(500).json({ error: "Server error" });
+    }
+});
+
+requestTasks.get("/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+        const requestTask = await getRequestTaskById(id);
+        if (requestTask) {
+            res.json(requestTask);
+        } else {
+            res.status(404).json({ error: "Tasks not found" });
+        }
+    } catch (error) {
+        res.status(500).json({ error: "Server error" });
+    }
+});
+
+module.exports = requestTasks;
