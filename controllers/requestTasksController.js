ee090c03f9ee51836490a97c16a72c6259c1e5a3
kurtCoding <kjulien7790@gmail.com>
2024-10-02 18:04:23 -0400

    added update and delete functionality to both requestTaskController and requestTaskQueries

--- controllers/requestTasksController.js
+++ controllers/requestTasksController.js
@@ -4,8 +4,8 @@ const {
     getAllRequestTasks,
     getRequestTaskById,
     createRequestTask,
-    // updateRequestTask,
-    // deleteRequestTask
+    updateRequestTask,
+    deleteRequestTask
 } = require("../queries/requestTasksQueries");
 
 requestTasks.get("/", async (req, res) => {
@@ -35,6 +35,34 @@ requestTasks.post("/", async (req, res) => {
     try {
         const newRequestTask = await createRequestTask(req.body);
         res.status(201).json(newRequestTask);
+    } catch (error) {
+        res.status(500).json({ error: "Server error" });
+    }
+});
+
+requestTasks.put("/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+        const updatedRequestTask = await updateRequestTask(id, req.body);
+        if (updatedRequestTask) {
+            res.status(200).json(updatedRequestTask);
+        } else {
+            res.status(404).json({ error: "Task not found" });
+        }
+    } catch (error) {
+        res.status(500).json({ error: "Server error" });
+    }
+});
+
+requestTasks.delete("/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+        const deletedRequestTask = await deleteRequestTask(id);
+        if (deletedRequestTask) {
+            res.status(200).json(deletedRequestTask);
+        } else {
+            res.status(404).json({ error: "Task not found" });
+        }
     } catch (error) {
         res.status(500).json({ error: "Server error" });
     }
--- queries/requestTasksQueries.js
+++ queries/requestTasksQueries.js
@@ -36,10 +36,47 @@ const createRequestTask = async (requestTask) => {
     }
 };
 
+const updateRequestTask = async (id, requestTask) => {
+    const {
+        requester_id,
+        organization_id,
+        request_id,
+        point_earnings,
+        due_date,
+    } = requestTask;
+
+    try {
+        const updatedRequestTask = await db.one(
+            "UPDATE request_task SET requester_id=$1, organization_id=$2, request_id=$3, point_earnings=$4 due_date=$5 WHERE id=$6 RETURNING *", [
+                requester_id,
+                organization_id,
+                request_id,
+                point_earnings,
+                due_date,
+                id,
+            ]
+        );
+        return updatedRequestTask;
+    } catch (error) {
+        throw error;
+    }
+};
+
+const deleteRequestTask = async (id) => {
+    try {
+        const deletedRequestTask = await db.one(
+            "DELETE FROM request_task WHERE id=$1 RETURNING *", id
+        );
+        return deletedRequestTask;
+    } catch (error) {
+        throw error;
+    }
+};
+
 module.exports = {
     getAllRequestTasks,
     getRequestTaskById,
     createRequestTask,
-    // updateRequestTask,
-    // deleteRequestTask
+    updateRequestTask,
+    deleteRequestTask
 }
