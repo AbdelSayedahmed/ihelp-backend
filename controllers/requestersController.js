7a64fbf9892d53cdf127a67f679a25182af3612b
AbdelSayedahmed <abdelrahman.s2003@icloud.com>
2024-10-05 15:22:55 -0400

    updated requesters controller

--- controllers/requestersController.js
+++ controllers/requestersController.js
@@ -19,10 +19,11 @@ requesters.get("/", async (req, res) => {
 
 requesters.get("/:id", async (req, res) => {
   const { id } = req.params;
+
   try {
     const requester = await getRequesterById(id);
     if (requester) {
-      res.json(requester);
+      res.status(200).json(requester);
     } else {
       res.status(404).json({ error: "Requester not found" });
     }
@@ -32,6 +33,10 @@ requesters.get("/:id", async (req, res) => {
 });
 
 requesters.post("/", async (req, res) => {
+  const { name, email } = req.body;
+
+  if (!name || !email) return res.status(400).json({ error: "Missing required fields" });
+
   try {
     const newRequester = await createRequester(req.body);
     res.status(201).json(newRequester);
@@ -40,12 +45,16 @@ requesters.post("/", async (req, res) => {
   }
 });
 
-requesters.delete("/:id", async (req, res) => {
+requesters.put("/:id", async (req, res) => {
   const { id } = req.params;
+  const { name, email } = req.body;
+
+  if (!name || !email) return res.status(400).json({ error: "Missing required fields" });
+
   try {
-    const deletedRequester = await deleteRequester(id);
-    if (deletedRequester) {
-      res.status(200).json(deletedRequester);
+    const updatedRequester = await updateRequester(id, req.body);
+    if (updatedRequester) {
+      res.status(200).json(updatedRequester);
     } else {
       res.status(404).json({ error: "Requester not found" });
     }
@@ -54,12 +63,13 @@ requesters.delete("/:id", async (req, res) => {
   }
 });
 
-requesters.put("/:id", async (req, res) => {
+requesters.delete("/:id", async (req, res) => {
   const { id } = req.params;
+
   try {
-    const updatedRequester = await updateRequester(id, req.body);
-    if (updatedRequester) {
-      res.status(200).json(updatedRequester);
+    const deletedRequester = await deleteRequester(id);
+    if (deletedRequester) {
+      res.status(200).json(deletedRequester);
     } else {
       res.status(404).json({ error: "Requester not found" });
     }
