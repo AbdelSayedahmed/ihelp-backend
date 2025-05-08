f9dd1d9d0e55113823413dc067b4544269e2ad1e
antonio <antonio.fr.martinezc@hotmail.com>
2025-05-08 11:37:08 -0400

    fixed env variable import to use commonjs

--- firebase-admin.js
+++ firebase-admin.js
@@ -2,7 +2,9 @@ const admin = require("firebase-admin");
 const serviceAccount = require("./firebase-credentials.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+	credential: admin.credential.cert(
+		serviceAccount || process.env.FIREBASE_CREDENTIALS
+	),
 });
 
 module.exports = admin;
--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 // DEPENDENCIES
 const app = require("./app.js");
 const http = require("http");
-const { initializeSocket } = require("./socket.js");
+// const { initializeSocket } = require("./socket.js");
 
 // CONFIGURATION
 require("dotenv").config();
 const PORT = process.env.PORT;
 
 const server = http.createServer(app);
-const io = initializeSocket(server);
+// const io = initializeSocket(server);
 // LISTEN
 server.listen(PORT, "0.0.0.0", () =>
 	console.log(`Listening on port http://0.0.0.0:${PORT}`)
